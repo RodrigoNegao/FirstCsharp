dbb5a44e018314b41ef7a4e1f64a5042358f8af9
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2019-12-04 17:22:28 -0300

    Testes1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Teste1");
+            Console.WriteLine("Teste2");
+
+
+
+            for (int i = 0; i =< 10; i++)
+            {
+                Console.WriteLine("Count {0} ", i);
+            }
+
+            // Get an array of the command line arguments
+            string[] myArgs = Environment.GetCommandLineArgs();
+
+            // Use the Join function to combine arguments with a comma
+            Console.WriteLine(string.Join(", ", myArgs));
+
+            SayHello();
+
+            Console.ReadLine();
+
+        }
+
+        // You can create your own functions (methods)
+        private static void SayHello()
+        {
+            // Defines a variable that will store a string
+            // of characters
+            string name = "";
+
+            Console.Write("What is your name : ");
+
+            // Save the input the user provides
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}", name);
         }
     }
 }
